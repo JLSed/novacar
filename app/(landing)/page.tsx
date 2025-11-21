0dba1b0e81025d78d618383a37660a362f9c2376
JLSed <seddd001@gmail.com>
2025-11-21 21:08:39 +0800

    added responsiveness

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -12,19 +12,19 @@ export default function Home() {
             src="/landing_bg1.png"
             alt="Landing Background"
             fill
-            className="object-cover z-0"
+            className="object-cover"
           />
         </AspectRatio>
       </div>
-      <div className="pt-32 relative w-full max-w-desktop flex flex-col items-center gap-medium">
-        <h1 className="text-center text-primary">
+      <div className="pt-32 max-md:pt-16 relative w-full max-w-desktop flex flex-col items-center gap-medium max-md:gap-small">
+        <h1 className="text-center text-primary ">
           <span className="text-accent2">Find</span> the right car.
         </h1>
-        <h6 className="text-center max-w-[582px]">
+        <h6 className="text-center max-w-[582px] max-md:max-w-[85%]">
           Unleash the thrill. Discover a curated collection of sports cars and
           iconic JDM legends.
         </h6>
-        <Link href={"/browse"} className="">
+        <Link href={"/browse"} className="max-md:scale-85">
           <button className="shining_button w-full">BROWSE &#8594;</button>
         </Link>
       </div>
